6b783485089e47c80e672bbb3cf3253a4a1067c8
kubus2731 <kubamatusiewicz65@gmail.com>
2025-06-02 01:12:51 +0200

    Dodano karuzele do skryptu

--- scripts/getProduct.js
+++ scripts/getProduct.js
@@ -14,19 +14,43 @@ console.log(array);
 const searchParams = new URLSearchParams(window.location.search);
 
 for (let i = 0; i < array.length; i++) {
-    if (searchParams.get('id')==i)
-    {
+    if (searchParams.get('id') == i) {
         let obj = array[i];
 
         const priceDiv = document.getElementById("price");
-        priceDiv.innerHTML = obj.price;
+        priceDiv.innerHTML = obj.price + "zł";
 
         const headerDiv = document.getElementById("productHeader");
         headerDiv.innerHTML = obj.name;
 
-        const imageAnchor = document.getElementById("imageAnchor");
-        imageAnchor.setAttribute("src","images\\"+obj.id+".webp");
-        imageAnchor.setAttribute("alt","Obrazek przedstawiający "+obj.name);
+        const leftProduct = document.getElementById("leftProduct");
+
+        const images = obj.images && obj.images.length ? obj.images : ["images/" + obj.id + ".webp"];
+        box.innerHTML = `
+            <div id="carousel" class="carousel">
+                <button id="prevBtn" class="carousel-btn" aria-label="Poprzednie zdjęcie"> < </button>
+                <img id="carouselImage" src="" alt="Obrazek przedstawiający ${obj.name}">
+                <button id="nextBtn" class="carousel-btn" aria-label="Następne zdjęcie"> > </button>
+            </div>
+        `;
+        let current = 0;
+        const img = document.getElementById('carouselImage');
+        const prev = document.getElementById('prevBtn');
+        const next = document.getElementById('nextBtn');
+
+        function showImage(idx) {
+            img.src = images[idx];
+            img.alt = "Obrazek przedstawiający " + obj.name;
+        }
+        prev.addEventListener('click', function() {
+            current = (current - 1 + images.length) % images.length;
+            showImage(current);
+        });
+        next.addEventListener('click', function() {
+            current = (current + 1) % images.length;
+            showImage(current);
+        });
+        showImage(current);
 
         const descriptionParagraph = document.getElementById("descrP");
         descriptionParagraph.innerHTML = obj.description;
@@ -37,13 +61,12 @@ for (let i = 0; i < array.length; i++) {
             const element = detailsArray[j];
             const splitElement = detailsArray[j].split(":");
             const trAnchor = document.createElement("tr");
-            trAnchor.innerHTML = "<td>"+splitElement[0]+"</td> <td>"+splitElement[1]+"</td>"; 
+            trAnchor.innerHTML = '<td id="cat" >' + splitElement[0]+":" + '</td> <td id="type" >' + splitElement[1] + "</td>";
             detailsTab.appendChild(trAnchor);
         }
         const detailsDiv = document.getElementById("detailsDiv");
         detailsDiv.appendChild(detailsTab);
     }
-
 }
 
 }
